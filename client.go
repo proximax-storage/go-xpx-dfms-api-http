b8d4cb51a16abd91a37ecd5d996802cf80dd2584
Hlib <Glebasya2000@mail.ru>
2019-09-02 18:54:42 +0300

    DFMS Http client architecture with DriveAPI Add

---
+++ client.go
@@ -0,0 +1,46 @@
+package httpclient
+
+import (
+	"net/http"
+)
+
+var (
+	APIPath = "/api/v1"
+)
+
+type Client struct {
+	url  string
+	http http.Client
+
+	Headers http.Header
+}
+
+func New(url string) (*Client, error) {
+	return NewWithClient(url, http.DefaultClient)
+}
+
+func NewWithClient(url string, http *http.Client) (*Client, error) {
+	client := &Client{
+		url:     url,
+		http:    *http,
+		Headers: make(map[string][]string),
+	}
+
+	return client, nil
+}
+
+func (api *Client) Request(command string, args ...string) RequestBuilder {
+	headers := make(map[string]string)
+	if api.Headers != nil {
+		for k := range api.Headers {
+			headers[k] = api.Headers.Get(k)
+		}
+	}
+
+	return &requestBuilder{
+		command: command,
+		args:    args,
+		client:  api,
+		headers: headers,
+	}
+}
