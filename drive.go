3246e1c5ee1c4ad5e0aed2956f5b31a9e7098096
Hlib <Glebasya2000@mail.ru>
2019-09-06 02:48:17 +0300

    DriveAPI implemented

--- drive.go
+++ drive.go
@@ -2,6 +2,7 @@ package httpclient
 
 import (
 	"context"
+	"os"
 
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
@@ -17,27 +18,68 @@ func (api *DriveAPI) Add(ctx context.Context, id cid.Cid, path string, file file
 	d := files.NewMapDirectory(map[string]files.Node{"": file}) // unwrapped on the other side inside commands
 	req.Body(files.NewMultiFileReader(d, false))
 
-	resp, err := req.Send(ctx)
-	if err != nil {
-		return cid.Undef, err
-	}
-	if resp.Error != nil {
-		return cid.Undef, resp.Error
-	}
-	defer resp.Output.Close()
-
-	var out addResponse
-	err = resp.decode(&out)
+	out := &addResponse{}
+	err := req.Exec(ctx, out)
 	if err != nil {
 		return cid.Undef, err
 	}
 
-	id, err = cid.Decode(out.Cid)
-	if err != nil {
-		return cid.Undef, err
-	}
+	return cid.Decode(out.Cid)
+}
 
-	return id, nil
+func (api *DriveAPI) Remove(ctx context.Context, id cid.Cid, path string) error {
+	return api.client().Request("drive/remove").
+		Arguments(id.String()).
+		Arguments(path).
+		Exec(ctx, nil)
+}
+
+func (api *DriveAPI) Move(ctx context.Context, id cid.Cid, src string, dst string) error {
+	return api.client().Request("drive/mv").
+		Arguments(id.String()).
+		Arguments(src).
+		Arguments(dst).
+		Exec(ctx, nil)
+}
+
+func (api *DriveAPI) Copy(ctx context.Context, id cid.Cid, src string, dst string) error {
+	return api.client().Request("drive/cp").
+		Arguments(id.String()).
+		Arguments(src).
+		Arguments(dst).
+		Exec(ctx, nil)
+}
+
+func (api *DriveAPI) MakeDir(ctx context.Context, id cid.Cid, path string) error {
+	return api.client().Request("drive/mkdir").
+		Arguments(id.String()).
+		Arguments(path).
+		Exec(ctx, nil)
+}
+
+func (api *DriveAPI) Ls(ctx context.Context, id cid.Cid, path string) ([]os.FileInfo, error) {
+	out := &lsResponse{}
+	err := api.client().Request("drive/ls").
+		Arguments(id.String()).
+		Arguments(path).
+		Exec(ctx, out)
+	return out.toFileInfo(), err
+}
+
+func (api *DriveAPI) Stat(ctx context.Context, id cid.Cid, path string) (os.FileInfo, error) {
+	out := &statResponse{}
+	err := api.client().Request("drive/stat").
+		Arguments(id.String()).
+		Arguments(path).
+		Exec(ctx, out)
+	return out.toFileInfo(), err
+}
+
+func (api *DriveAPI) Flush(ctx context.Context, id cid.Cid, path string) error {
+	return api.client().Request("drive/flush").
+		Arguments(id.String()).
+		Arguments(path).
+		Exec(ctx, nil)
 }
 
 func (api *DriveAPI) client() *Client {
@@ -46,4 +88,25 @@ func (api *DriveAPI) client() *Client {
 
 type addResponse struct {
 	Cid string
+}
+
+type lsResponse struct {
+	List []*Stat
+}
+
+func (ls *lsResponse) toFileInfo() []os.FileInfo {
+	fis := make([]os.FileInfo, len(ls.List))
+	for i, entry := range ls.List {
+		fis[i] = &FileInfo{*entry}
+	}
+
+	return fis
+}
+
+type statResponse struct {
+	Stat *Stat
+}
+
+func (ls *statResponse) toFileInfo() os.FileInfo {
+	return &FileInfo{*ls.Stat}
 }
---
+++ fileinfo.go
@@ -0,0 +1,43 @@
+package httpclient
+
+import (
+	"os"
+	"time"
+
+	"github.com/ipfs/go-cid"
+)
+
+type Stat struct {
+	Name string
+	Size int64
+	Type string
+	Cid  cid.Cid
+}
+
+type FileInfo struct {
+	stat Stat
+}
+
+func (ref *FileInfo) Name() string {
+	return ref.stat.Name
+}
+
+func (ref *FileInfo) Size() int64 {
+	return ref.stat.Size
+}
+
+func (ref *FileInfo) Mode() os.FileMode {
+	return 0
+}
+
+func (ref *FileInfo) ModTime() time.Time {
+	return time.Time{}
+}
+
+func (ref *FileInfo) IsDir() bool {
+	return ref.stat.Type == "dir" // Taken from MFS
+}
+
+func (ref *FileInfo) Sys() interface{} {
+	return &ref.stat
+}
