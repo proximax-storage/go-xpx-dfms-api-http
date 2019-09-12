97e956f55b96137f8eeeddc43d0b235aa24dd8d2
Evgeny Ukhanov <mrlsd@ya.ru>
2019-09-12 12:21:22 +0300

    Added Contract commands

--- contract.go
+++ contract.go
@@ -2,20 +2,38 @@ package httpclient
 
 import (
 	"context"
+	"time"
+
 	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 type ContractAPI Client
 
-func (api *ContractAPI) Get(ctx context.Context, id cid.Cid) (cid.Cid, error) {
-	out := &getContractResponse{}
+type DriveInvite struct {
+	Cid           cid.Cid
+	Created       time.Time
+	Duration      time.Duration
+	RequiredSpace uint64
+	Owner         peer.ID
+}
+
+type ContractResponse struct {
+	Cid        cid.Cid
+	Owner      peer.ID
+	Members    []peer.ID
+	Duration   time.Duration
+	Created    time.Time
+	Root       cid.Cid
+	TotalSpace uint64
+}
+
+func (api *ContractAPI) Get(ctx context.Context, id cid.Cid) (*ContractResponse, error) {
+	out := &ContractResponse{}
 	err := api.client().Request("contract/get").
 		Arguments(id.String()).
 		Exec(ctx, out)
-	if err != nil {
-		return cid.Undef, err
-	}
-	return cid.Decode(out.Cid)
+	return out, err
 }
 
 func (api *ContractAPI) Join(ctx context.Context, id cid.Cid) error {
@@ -24,17 +42,44 @@ func (api *ContractAPI) Join(ctx context.Context, id cid.Cid) error {
 		Exec(ctx, nil)
 }
 
-func (api *ContractAPI) List(ctx context.Context, id cid.Cid) ([]cid.Cid, error) {
+func (api *ContractAPI) List(ctx context.Context) ([]cid.Cid, error) {
 	out := &listContractResponse{}
 	err := api.client().Request("contract/list").
-		Arguments(id.String()).
 		Exec(ctx, out)
 	return out.Cids, err
 }
 
-func (api *ContractAPI) Updates(ctx context.Context, id cid.Cid) error {
-	return api.client().Request("contract/updates").
+func (api *ContractAPI) Updates(ctx context.Context, id cid.Cid) (*ContractResponse, error) {
+	out := &ContractResponse{}
+	err := api.client().Request("contract/updates").
 		Arguments(id.String()).
+		Exec(ctx, out)
+	return out, err
+}
+
+func (api *ContractAPI) Compose(ctx context.Context, space uint64, duration time.Duration) (*ContractResponse, error) {
+	out := &ContractResponse{}
+	err := api.client().Request("contract/compose").
+		Arguments(string(space)).
+		Arguments(string(duration)).
+		Exec(ctx, out)
+	return out, err
+}
+
+func (api *ContractAPI) Invites(ctx context.Context) (*DriveInvite, error) {
+	out := &DriveInvite{}
+	err := api.client().Request("contract/invites").
+		Exec(ctx, out)
+	return out, err
+}
+
+func (api *ContractAPI) StartAccepting(ctx context.Context) error {
+	return api.client().Request("contract/start-accepting").
+		Exec(ctx, nil)
+}
+
+func (api *ContractAPI) StopAccepting(ctx context.Context) error {
+	return api.client().Request("contract/stop-accepting").
 		Exec(ctx, nil)
 }
 
