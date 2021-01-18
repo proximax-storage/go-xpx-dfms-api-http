e269d8e59aa1301085706766ee38122c917ec354
Yevhen <iorgachov1998@gmail.com>
2021-01-18 19:49:09 +0200

    implement new Node interface from API

---
+++ node.go
@@ -0,0 +1,35 @@
+package apihttp
+
+import (
+	"context"
+	"net/http"
+
+	api "github.com/proximax-storage/go-xpx-dfms-api"
+)
+
+type node struct {
+	*apiHttp
+
+	tp api.NodeType
+}
+
+// newNodeAPI creates new *node from given address, http.Client and type
+func newNodeAPI(address string, client *http.Client, tp api.NodeType) *node {
+	return &node{
+		apiHttp: newHTTP(address, client),
+		tp:      tp,
+	}
+}
+
+func (n *node) Network() api.Network {
+	return (*apiNetwork)(n.apiHttp)
+}
+
+func (n *node) Type() api.NodeType {
+	return n.tp
+}
+
+func (n *node) Version(ctx context.Context) (*api.Versions, error) {
+	out := new(api.Versions)
+	return out, n.apiHttp.NewRequest("version").Exec(ctx, &out)
+}
